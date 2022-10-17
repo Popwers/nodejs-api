e3f66847b7b87675db0ee8c1b710043dd80cda61
Popwers <lionel.bataille@hotmail.com>
2022-10-17 15:24:22 +0400

    Optimise the server

--- index.js
+++ index.js
@@ -1,91 +1,90 @@
-const app = require("express")()
-const mysql = require("mysql")
-const http = require("http")
-const server = http.createServer(app)
-const io = require("socket.io")(server)
-/*const io = socketIo(server, {
-    cors: {
-        origin: '*'
-    }
-})*/
+const app = require('express')();
+const mysql = require('mysql');
+const { createServer } = require('http');
+const httpServer = createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(httpServer, {
+	serveClient: false,
+	/*cors: {
+		origin: '*',
+	},*/
+});
 
 /* ******************************************************************* */
 /* *********************** MIDDLEWARE & ROUTES *********************** */
 /* ******************************************************************* */
 
-const { ioAuth, corsAuth } = require("./middleware/auth")
-const authRouter = require("./routes/auth")
-const animationsRouter = require("./routes/animations")
-const animateursRouter = require("./routes/animateurs")
-const clientsRouter = require("./routes/clients")
-const lieuxRouter = require("./routes/lieux")
+const { ioAuth, corsAuth } = require('./middleware/auth');
+const authRouter = require('./routes/auth');
+const animationsRouter = require('./routes/animations');
+const animateursRouter = require('./routes/animateurs');
+const clientsRouter = require('./routes/clients');
+const lieuxRouter = require('./routes/lieux');
 
 /* ********************************************************* */
 /* *********************** DATABASE ************************ */
 /* ********************************************************* */
 
 const db_config = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-}
-var db = null
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+};
+var db = null;
 
 function handleDisconnect() {
-    db = mysql.createConnection(db_config)
-    db.connect((err) => {
-        if (err) {
-            console.log('> Error when connecting to Database : ', err)
-            setTimeout(handleDisconnect, 2000)
-        } else console.log("> SQL Database Ready")
-    })
+	db = mysql.createConnection(db_config);
+	db.connect(err => {
+		if (err) {
+			console.log('> Error when connecting to Database : ', err);
+			setTimeout(handleDisconnect, 2000);
+		} else console.log('> SQL Database Ready');
+	});
 
-    db.on('error', function (err) {
-        console.log('> Database Error : ', err)
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.log('> Reconnecting to Database...')
-            handleDisconnect()
-        }
-        else throw err
-    })
+	db.on('error', function (err) {
+		console.log('> Database Error : ', err);
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			console.log('> Reconnecting to Database...');
+			handleDisconnect();
+		} else throw err;
+	});
 }
 
-handleDisconnect()
+handleDisconnect();
 setInterval(() => {
-    db !== null ? db.query('SELECT 1') : null
-}, 5000)
+	db !== null ? db.query('SELECT 1') : null;
+}, 5000);
 
 /* **************************************************************** */
 /* *************************** API ROUTES ************************* */
 /* **************************************************************** */
 
-app.set("io", io)
-    .use(corsAuth)
-    .use("/auth", authRouter(db))
-    .use("/api", animationsRouter(app, db))
-    .use("/api", animateursRouter(app, db))
-    .use("/api", clientsRouter(app, db))
-    .use("/api", lieuxRouter(app, db))
-    .route("*").all((req, res, next) => {
-        res.sendStatus(404)
-    })
-
+app.set('io', io)
+	.use(corsAuth)
+	.use('/auth', authRouter(db))
+	.use('/api', animationsRouter(app, db))
+	.use('/api', animateursRouter(app, db))
+	.use('/api', clientsRouter(app, db))
+	.use('/api', lieuxRouter(app, db))
+	.route('*')
+	.all((req, res, next) => {
+		res.sendStatus(404);
+	});
 
 /* **************************************************************** */
 /* *************************** SOCKET IO ************************** */
 /* **************************************************************** */
 
-io.on('connection', (socket) => {
-    ioAuth(socket, io)
-})
-
+io.on('connection', socket => {
+	ioAuth(socket, io);
+});
 
 /* **************************************************************** */
 /* *************************** SERVER START ************************ */
 /* **************************************************************** */
 
-server.listen('passenger', (err) => {
-    if (err) throw err
-    console.log(`> Server Ready`)
-})
+httpServer.listen('passenger', err => {
+	if (err) throw err;
+	console.log(`> Server Ready`);
+});
